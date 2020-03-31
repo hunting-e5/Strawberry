b549a76cd02a1a8edede11257e3698b849e5dd66
tylerkim1 <61447161+tylerkim1@users.noreply.github.com>
2020-03-31 20:48:01 +0900

    V 0.0.2

--- src/components/Header/Login.js
+++ src/components/Header/Login.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-        },
-    },
-}));
 
 function Login() {
-    const classes = useStyles();
-
     return (
-        <span id = 'login'>
-            <span className={classes.root}>
-                <Button>로그인</Button>
-            </span>
+        <span id = 'login_wrap'>
+            <button>로그인</button>
+            <button>회원가입</button>
         </span>
     );
 }
--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -3,9 +3,11 @@ import './Header.css'
 
 function Logo() {
     return (
-        <a href="http://localhost:3000/">
-            <img id="logo" alt="strawberry" src="https://avatars2.githubusercontent.com/u/61527805?s=200&v=4" />
-        </a>
+        <span id = "logo_wrap">
+            <a href="http://localhost:3000/">
+                <img id="logo" alt="strawberry" src="https://avatars2.githubusercontent.com/u/61527805?s=200&v=4" />
+            </a>
+        </span>
     );
 }
 
