200ae0352bdc931db08a38fb59f140f1913eb110
Gyubin-Lee <leegyubin00@gmail.com>
2020-04-18 06:11:51 +0900

    create tap

--- client/src/components/Tabs/CreateTab/CreateSupplyPost.js
+++ client/src/components/Tabs/CreateTab/CreateSupplyPost.js
@@ -2,7 +2,17 @@
 
 import React, { Component } from 'react';
 
+const axios = require('axios').default;
+
 class CreatePost extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+    
+    
     render() {
         return (
             <div>
